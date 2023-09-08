239066e90a80425322d6aebe515388a6f804d9c5
Arturo Bernal <arturo@email.com>
2023-09-08 11:12:23 +0200

    port

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as express from 'express';
 import { useContainer } from 'class-validator';
+import appConfig from './app.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
   app.use('/img', express.static('img'));
   app.use(express.json({ limit: '10mb' }));
   useContainer(app.select(AppModule), { fallbackOnErrors: true });
-  await app.listen(3000);
+  await app.listen(appConfig().port);
 }
 bootstrap();
