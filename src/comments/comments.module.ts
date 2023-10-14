a1b84cfb26351f726f450c57fca3b135d9926973
Arturo Bernal <arturo@email.com>
2023-10-14 23:27:20 +0200

    bug comments

--- src/comments/comments.module.ts
+++ src/comments/comments.module.ts
@@ -6,12 +6,11 @@ import { MikroOrmModule } from '@mikro-orm/nestjs';
 import { User } from 'src/users/entities/user.entity';
 import { Post } from 'src/posts/entities/post.entity';
 import { Comment } from './entities/comment.entity';
-import { CommentResponseInterceptor } from './interceptors/comment-response.interceptor';
 
 @Module({
   imports: [MikroOrmModule.forFeature([Comment, User, Post]), CommonsModule],
   controllers: [CommentsController],
   providers: [CommentsService],
-  exports: [CommentsService, CommentResponseInterceptor],
+  exports: [CommentsService],
 })
 export class CommentsModule {}
